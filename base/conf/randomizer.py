d50fa0385f5d747108cd48bedaf0731f6a043c10
federico <legofed3@hotmail.com>
2016-03-10 15:09:22 +0100

    improved randomizer script to have UNIX-like behavior (see usage)

--- base/conf/randomizer.py
+++ base/conf/randomizer.py
@@ -1,8 +1,18 @@
 import xml.etree.ElementTree as ET
 import random
+import sys
+
+if len(sys.argv) < 3 or len(sys.argv) > 4:
+	print 'Usage: python randomizer.py input.xml output.xml [seed]'
+	exit(-1)
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+if len(sys.argv) == 4:
+	random.seed(sys.argv[3])
 
 ET.register_namespace('', "http://sndlib.zib.de/network")
-tree = ET.parse('nobel-us.xml')
+tree = ET.parse(infile)
 root = tree.getroot()
 
 demandValues = []
@@ -15,4 +25,4 @@ random.shuffle(demandValues)
 for i in range(len(demandValues)):
 	root[1][i][2].text = demandValues[i]
 
-tree.write('out.xml')
+tree.write(outfile)
