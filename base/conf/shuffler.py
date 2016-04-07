b0b3471fb7a7f80fbd438b46c46017fa0a9fed08
federico <legofed3@hotmail.com>
2016-04-07 10:07:33 +0200

    Renamed randomizer to shuffler, implemented new randomizer to actually just randomize values using a normal distribution with avg 0 and stddel euqal to sqrt of three times the original value (truncated to positive)

--- base/conf/shuffler.py
+++ base/conf/shuffler.py
@@ -1,7 +1,6 @@
 import xml.etree.ElementTree as ET
 import random
 import sys
-import math
 
 if len(sys.argv) < 3 or len(sys.argv) > 4:
 	print 'Usage: python randomizer.py input.xml output.xml [seed]'
@@ -16,13 +15,14 @@ ET.register_namespace('', "http://sndlib.zib.de/network")
 tree = ET.parse(infile)
 root = tree.getroot()
 
+demandValues = []
 #demands are in root[1]
 for demand in root[1]:
-	value = float(demand[2].text)
-	randomizedValue = value + random.normalvariate(0, math.sqrt(3*value))
-#	print "Value is "+str(value)+", randomized to "+str(randomizedValue)+", difference is "+str(value - randomizedValue)
-	if (randomizedValue < 0):
-		randomizedValue = 1.0;
-	demand[2].text = str(randomizedValue)
+	demandValues.append(demand[2].text)
+
+random.shuffle(demandValues)
+
+for i in range(len(demandValues)):
+	root[1][i][2].text = demandValues[i]
 
 tree.write(outfile)
